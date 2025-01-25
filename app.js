3bd5d87a4b82e29fe1e09ad806e79338a33cd239
irene <wunder.frau@yandex.by>
2025-01-26 01:29:46 +0200

    update the routes so that they match the /api prefix

--- app.js
+++ app.js
@@ -85,9 +85,9 @@ app.get("/crash-test", () => {
   }, 0);
 });
 
-app.post("/signin", loginValid, login);
+app.post("api/signin", loginValid, login);
 
-app.post("/signup", createUserValid, createUser);
+app.post("api/signup", createUserValid, createUser);
 
 app.use(auth);
 app.use("/", cardsRouter);
